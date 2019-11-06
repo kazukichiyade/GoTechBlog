9dc025f8fa3a2d54971e8d2a38d0a0dcdca1fc92
kazukichi <zexyl_x@icloud.com>
2019-11-06 22:21:26 +0900

    Prism.css,Prism.js導入

--- src/js/module/parser.js
+++ src/js/module/parser.js
@@ -4,5 +4,9 @@ const { Remarkable } = window.remarkable;
 
 const md = new Remarkable({
   html: true,
-  breaks: true
+  breaks: true,
+  langPrefix: 'language-',
+  highlight: function(str, lang) {
+    return Prism.highlight(str, Prism.languages[lang], lang);
+  }
 });
