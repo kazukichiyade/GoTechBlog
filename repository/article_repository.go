de5b5af3a818eaa51ce8e4b08bd28e814139185d
kazukichi <zexyl_x@icloud.com>
2019-11-09 21:36:09 +0900

    article_repositoryのArticleListByCursor処理追加

--- repository/article_repository.go
+++ repository/article_repository.go
@@ -4,23 +4,42 @@ import (
 	// model パッケージで定義した Article 構造体を利用するため、パッケージをインポート
 	"database/sql"
 	"go-tech-blog/model"
+	"math"
 	"time"
 )
 
 /* 戻り値ありの関数 */
-func ArticleList() ([]*model.Article, error) {
-	// 実行する SQL
-	query := `SELECT * FROM articles;`
+// func ArticleList() ([]*model.Article, error) {
+// 	// 実行する SQL
+// 	query := `SELECT * FROM articles;`
 
-	// データベースから取得した値を格納する変数を宣言
-	var articles []*model.Article
+// 	// データベースから取得した値を格納する変数を宣言
+// 	var articles []*model.Article
 
-	// Query を実行して、取得した値を変数に格納
-	if err := db.Select(&articles, query); err != nil {
-		return nil, err
+// 	// Query を実行して、取得した値を変数に格納
+// 	if err := db.Select(&articles, query); err != nil {
+// 		return nil, err
+// 	}
+
+// 	return articles, nil
+// }
+
+func ArticleListByCursor(cursor int) ([]*model.Article, error) {
+	// 引数で渡されたカーソルの値が 0 以下の場合は、代わりに int 型の最大値で置き換え
+	if cursor <= 0 {
+		cursor = math.MaxInt32
 	}
 
-	return articles, nil
+	// ID の降順に記事データを 10 件取得するクエリ文字列を生成
+	query := `SELECT *
+	FROM articles
+	WHERE id < ?
+	ORDER BY id desc
+	LIMIT 10`
+
+	// クエリ結果を格納するスライスを初期化
+	// 10 件取得すると決まっているため、サイズ(要素数)とキャパシティ(容量)を指定
+	articles := make([]*model.Article, 0, 10)
 }
 
 func ArticleCreate(article *model.Article) (sql.Result, error) {
